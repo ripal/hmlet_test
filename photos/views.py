ef0334ebb0f557d05866609382e6e647fcae2a09
ripal <v4u.ripal@gmail.com>
2020-01-26 15:00:20 +0530

    ADD: django-filter deps

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from . import models
 from . import utils
@@ -6,3 +7,6 @@ from . import utils
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = models.Photo.objects.all()
     serializer_class = utils.PhotoSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['status']
+    ordering_fields = ['changed_on']
