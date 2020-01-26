444dca127f32520e197d6ba771f7c809e677c1e9
ripal <v4u.ripal@gmail.com>
2020-01-26 15:04:56 +0530

    FIX: OrderFilter import from wrong package

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 from . import models
 from . import utils
 
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = models.Photo.objects.all()
     serializer_class = utils.PhotoSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['status']
     ordering_fields = ['changed_on']
