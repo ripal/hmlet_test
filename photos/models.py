7c24b1e11197ba80446924a66ba17704b783b9de
ripal <v4u.ripal@gmail.com>
2020-01-26 13:35:32 +0530

    ADD: photo model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+
+    caption = models.CharField(max_length = 255, verbose_name = u'Photo_Caption')
+    status = models.CharField(max_length = 255, verbose_name = u'Photo_Status')
+    url = models.CharField(max_length = 500, verbose_name = u'Photo_Url')
+    changed_on = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '%s' % (self.name)
+
+    class Meta:
+        ordering = ('changed_on', '-changed_on')
